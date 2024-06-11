2f6cf91de97c4ecc87af300507df3e3289dc2038
sofiacottone <cottonesofia@outlook.it>
2024-06-11 15:50:18 +0200

    feat: add show method to create single project api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,11 +10,30 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::with('type', 'technologies')->get();
+        $projects = Project::with('type', 'technologies')->paginate(3);
 
         return response()->json([
             'success' => true,
             'results' => $projects
         ]);
+    }
+
+    public function show($slug)
+    {
+        $project = Project::where('slug', '=', $slug)->with('type', 'technologies')->first();
+
+        if ($project) {
+            $data = [
+                'success' => true,
+                'results' => $project
+            ];
+        } else {
+            $data = [
+                'success' => false,
+                'error' => 'No projects found with this slug'
+            ];
+        }
+
+        return response()->json($data);
     }
 }
